6a9a53500f8da26a502e3fa939a53841426d04c3
wfxt <812552849@qq,com>
2022-06-16 10:46:24 +0800

    Site updated: 2022-06-16 10:46:24

---
+++ dist/music.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+        {
+            name: '����',
+            artist: '����',
+            url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
+            cover: '/images/5.jpg',
+        },
+        {
+            name: "����",
+            artist: '�Ž�',
+            url: 'http://music.163.com/song/media/outer/url?id=1399004693.mp3',
+            cover: '/images/xingxing.jpg',
+        },
+        {
+            name: '����ǰ���',
+            artist: 'ʮ��',
+            url: 'http://music.163.com/song/media/outer/url?id=1412242872.mp3',
+            cover: '/images/zjsam.jpg',
+        },
+        {
+            name: 'Alone',
+            artist: 'Alan Walker / Noonie Bao',
+            url: 'http://music.163.com/song/media/outer/url?id=444269135.mp3',
+            cover: '/images/alone.jpg',
+        },
+        {
+            name: 'Umbrella (Matte Remix)',
+            artist: 'Matte / Ember Island',
+            url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
+            cover: '/images/1.jpg',
+        },
+        {
+            name: '���� (������)ԭ����������',
+            artist: 'С�ָ�',
+            url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
+            cover: '/images/2.jpg',
+        },
+        {
+            name: '�����������㻷�����',
+            artist: '����',
+            url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
+            cover: '/images/3.jpg',
+        },
+        {
+            name: '��',
+            artist: '������Est / ����u',
+            url: 'http://music.163.com/song/media/outer/url?id=1386259535.mp3',
+            cover: '/images/4.jpg',
+        }
+    ]
+});
