c582c4b74486ddec84f436191ef7d7c62ad63b98
wfxt <812552849@qq,com>
2022-06-16 11:44:57 +0800

    Site updated: 2022-06-16 11:44:57

--- dist/music.js
+++ dist/music.js
@@ -1,55 +1,37 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: true,
+    autoplay: false,
     audio: [
-        {
-            name: '����',
-            artist: '����',
-            url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
-            cover: '/images/5.jpg',
-        },
-        {
-            name: "����",
-            artist: '�Ž�',
-            url: 'http://music.163.com/song/media/outer/url?id=1399004693.mp3',
-            cover: '/images/xingxing.jpg',
-        },
-        {
-            name: '����ǰ���',
-            artist: 'ʮ��',
-            url: 'http://music.163.com/song/media/outer/url?id=1412242872.mp3',
-            cover: '/images/zjsam.jpg',
-        },
-        {
-            name: 'Alone',
-            artist: 'Alan Walker / Noonie Bao',
-            url: 'http://music.163.com/song/media/outer/url?id=444269135.mp3',
-            cover: '/images/alone.jpg',
-        },
-        {
-            name: 'Umbrella (Matte Remix)',
-            artist: 'Matte / Ember Island',
-            url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
-            cover: '/images/1.jpg',
-        },
-        {
-            name: '���� (������)ԭ����������',
-            artist: 'С�ָ�',
-            url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
-            cover: '/images/2.jpg',
-        },
-        {
-            name: '�����������㻷�����',
-            artist: '����',
-            url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
-            cover: '/images/3.jpg',
-        },
-        {
-            name: '��',
-            artist: '������Est / ����u',
-            url: 'http://music.163.com/song/media/outer/url?id=1386259535.mp3',
-            cover: '/images/4.jpg',
-        }
+	{
+        name: "ƽ��֮·",
+        artist: '����',
+        url: 'http://www.ytmp3.cn/down/59211.mp3',
+        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+      },
+	  {
+        name: '��Щ��ҥ - һ��������',
+        artist: '�̴�',
+        url: 'http://www.ytmp3.cn/down/60222.mp3',
+        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+      },
+      {
+        name: '��ľƹݶ��Ҵ�����',
+        artist: '��ѩ��',
+        url: 'http://www.ytmp3.cn/down/59770.mp3',
+        cover: 'http://p1.music.126.net/LiRR__0pJHSivqBHZzbMUw==/109951163816225567.jpg?param=130y130',
+      },
+	  {
+        name: 'Something Just Like This',
+        artist: 'The Chainsmokers',
+        url: 'http://www.ytmp3.cn/down/50463.mp3',
+        cover: 'http://p2.music.126.net/ggnyubDdMxrhpqYvpZbhEQ==/3302932937412681.jpg?param=130y130',
+      },
+      {
+        name: 'Good Time',
+        artist: 'Owl City&Carly Rae Jepsen',
+        url: 'http://www.ytmp3.cn/down/34148.mp3',
+        cover: 'http://p1.music.126.net/c5NVKUIAUcyN4BQUDbGnEg==/109951163221157827.jpg?param=130y130',
+      }
     ]
 });
